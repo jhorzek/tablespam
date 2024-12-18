a5201a0f6b37599a234c48e12106da2867cfc38e
Jannik Orzek <jannik.orzek@mailbox.org>
2024-12-18 23:05:13 +0100

    Added add_header_level function to add the header levels in the correct order

--- tablespam/Formulas.py
+++ tablespam/Formulas.py
@@ -17,8 +17,10 @@ class Formula:
         else:
             lhs = create_entries(parsed_formula[0])
             lhs = add_header_width(lhs)
+            lhs = add_header_level(lhs)
         rhs = create_entries(parsed_formula[1])
         rhs = add_header_width(rhs)
+        rhs = add_header_level(rhs)
 
         return {"lhs": lhs, "rhs": rhs}
     
@@ -45,8 +47,6 @@ def create_entries(entry_list: list[str], depth: int | None = None) -> list:
 
         header_entry = HeaderEntry(name = spanner_name, 
                                    item_name = spanner_name)
-        
-    header_entry.set_level(depth)
 
     for entry in entry_list:
         if isinstance(entry, str):
@@ -54,7 +54,6 @@ def create_entries(entry_list: list[str], depth: int | None = None) -> list:
             variable = split_variable(entry)
             sub_entry = HeaderEntry(name = variable["name"],
                         item_name = variable["item_name"])
-            sub_entry.set_level(depth + 1)
             header_entry.add_entry(sub_entry)
         elif isinstance(entry, list):
             header_entry.add_entry(create_entries(entry, depth = depth + 1))
@@ -167,4 +166,31 @@ def add_header_width(parsed_partial):
         parsed_partial.entries[entry_index] = add_header_width(parsed_partial.entries[entry_index])
         parsed_partial.set_width(parsed_partial.width + parsed_partial.entries[entry_index].width)
 
+    return parsed_partial
+
+def add_header_level(parsed_partial):
+    if parsed_partial is None:
+        return None
+
+    # Level 1 is the level of the headers that are closest
+    # to the data. When we have an empty entries list,
+    # we reached that level.
+    if len(parsed_partial.entries) == 0:
+        parsed_partial.set_level(1)
+        return parsed_partial
+    
+    parsed_partial.set_level(0)
+    for entry_index in range(len(parsed_partial.entries)):
+        # Bascially, what we are doing here is updating the level of one
+        # subentry at a time
+        parsed_partial.entries[entry_index] = add_header_level(parsed_partial.entries[entry_index])
+        # After updating the subentries, we check if the current level of the entry
+        # is lower than the subentry + 1. If this is the case, we need to update the level
+        # of the entry.
+        parsed_partial.set_level(max(parsed_partial.level,
+                                     parsed_partial.entries[entry_index].level + 1))
+        
+    if parsed_partial.level == 0:
+        raise ValueError(f"Could not set a level for {parsed_partial}")
+        
     return parsed_partial
